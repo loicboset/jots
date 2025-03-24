142869a5e7cb502c39997272de7e44237c105612
Loïc <loic@kaironlabs.com>
2025-03-24 08:30:15 +0100

    fix in-app latest version

--- src/app/api/last_version/route.ts
+++ src/app/api/last_version/route.ts
@@ -4,7 +4,7 @@ import path from "path";
 export async function GET(): Promise<Response> {
   const versionsDir = path.join(process.cwd(), "versions");
   const files = fs.readdirSync(versionsDir);
-  const lastFile = path.join(versionsDir, files[0]);
+  const lastFile = path.join(versionsDir, files[files.length - 1]);
   const lastVersion = JSON.parse(fs.readFileSync(lastFile, "utf8")).version;
 
   return new Response(JSON.stringify(lastVersion), { status: 200 });
