82eed38a914f37a88bbe855c58222bb046f632a5
Loïc <loic@kaironlabs.com>
2025-05-25 17:19:29 +0200

    debug

--- src/app/api/push_notifications/send/route.ts
+++ src/app/api/push_notifications/send/route.ts
@@ -13,11 +13,14 @@ export async function GET(request: Request): Promise<Response | undefined> {
   try {
     const supabase = await createClient();
 
-    const { data: push_subscriptions = [] } = await supabase
+    const { data: push_subscriptions = [], error } = await supabase
       .from("push_subscriptions")
       .select("auth, endpoint, p256dh")
       .eq("user_id", userID);
-
+    if (error) {
+      console.log(" error", error);
+      return new Response(`Error fetching push subscriptions: ${error}`, { status: 500 });
+    }
     if (!push_subscriptions || push_subscriptions.length === 0) {
       return new Response("No push subscriptions found", { status: 500 });
     }
