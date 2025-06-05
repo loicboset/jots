ada5aa0c6622a5243e315f0bffef7ff56306bf16
Julien Avezou <julien.avezou@gmail.com>
2025-06-05 22:37:26 +0200

    add mood chart

--- src/app/api/mood_checks/route.ts
+++ src/app/api/mood_checks/route.ts
@@ -3,6 +3,9 @@ import { createClient } from '@/lib/supabase/server';
 export async function GET(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url);
   const userID = searchParams.get('user_id');
+  const limitParam = searchParams.get('limit');
+
+  const limit = limitParam ? parseInt(limitParam, 10) : 3;
 
   const supabase = await createClient();
 
@@ -11,7 +14,7 @@ export async function GET(request: Request): Promise<Response> {
     .select('*')
     .eq('user_id', userID)
     .order('created_at', { ascending: false })
-    .limit(3);
+    .limit(limit);
 
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
