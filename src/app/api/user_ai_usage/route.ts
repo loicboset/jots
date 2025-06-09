fa57eaf8b84130875c03069a78d5d2c8ccf44c16
Loïc <loic@kaironlabs.com>
2025-06-09 20:38:05 +0200

    show ai tokens usage

---
+++ src/app/api/user_ai_usage/route.ts
@@ -0,0 +1,24 @@
+import { createClient } from "@/lib/supabase/server";
+
+import getUserID from "../_utils/getUserID";
+
+const MUTLIPLIER = 100_000;
+
+export async function GET(request: Request): Promise<Response> {
+  const { searchParams } = new URL(request.url);
+  const date = searchParams.get("date");
+
+  const userID = await getUserID();
+  if (!userID) return new Response("Unauthorized", { status: 401 });
+
+  const supabase = await createClient();
+  const { data: usage } = await supabase.from("user_ai_usage").select("cost").eq("user_id", userID).eq("date", date);
+
+  const currentCost = usage?.reduce((prev, curr) => prev + curr.cost, 0) || 0;
+  const usedTokens = Math.min(Math.floor(currentCost * MUTLIPLIER), 100);
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  return new Response(JSON.stringify(usedTokens), { status: 200, headers });
+}
---
+++ src/services/user_ai_usage.ts
@@ -0,0 +1,14 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import axios from "axios";
+
+// GET USER SETTINGS
+const getUserAiUsage = async (date: string): Promise<number> => {
+  const { data } = await axios.get(`/api/user_ai_usage?date=${date}`);
+  return data;
+};
+
+const useUserAiUsage = (date: string): UseQueryResult<number, Error> => {
+  return useQuery({ queryKey: ["user_ai_usage"], queryFn: () => getUserAiUsage(date) });
+};
+
+export { useUserAiUsage };
