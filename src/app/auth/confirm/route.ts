25701c242fb2356e5ec91b3aab19dd1faf5455c9
Loïc <loic@kaironlabs.com>
2025-07-02 15:30:18 +0200

    password reset

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,15 +1,14 @@
-import { type EmailOtpType } from '@supabase/supabase-js';
-import { redirect } from 'next/navigation';
-import { type NextRequest } from 'next/server';
+import { type EmailOtpType } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
 
-import { createClient } from '@/lib/supabase/server';
-
+import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: NextRequest): Promise<void> {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get('token_hash');
-  const type = searchParams.get('type') as EmailOtpType | null;
-  const next = searchParams.get('next') ?? '/';
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -25,5 +24,6 @@ export async function GET(request: NextRequest): Promise<void> {
   }
 
   // redirect the user to an error page with some instructions
-  redirect('/error');
+
+  redirect("/error");
 }
---
+++ src/app/reset-password/actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function resetPassword(newPassword: string): Promise<{ message: string; type: "success" | "danger" }> {
+  const supabase = await createClient();
+
+  const { error } = await supabase.auth.updateUser({ password: newPassword });
+
+  if (error) {
+    return { message: error?.message ?? "An error occurred. Please try again or contact support.", type: "danger" };
+  }
+
+  revalidatePath("/", "layout");
+
+  return { message: "Your password has been updated!", type: "success" };
+}
