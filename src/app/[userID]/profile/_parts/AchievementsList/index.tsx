171e25f94a8c4705964ebbc770aba499476ec4c3
Julien Avezou <julien.avezou@gmail.com>
2025-07-02 23:40:55 +0200

    improve hidden logic, fix typo

--- src/app/[userID]/profile/_parts/AchievementsList/index.tsx
+++ src/app/[userID]/profile/_parts/AchievementsList/index.tsx
@@ -10,12 +10,12 @@ const AchievementsList = (): React.ReactElement => {
       <div>
         <h2 className="text-base/7 font-semibold text-white">Achievements</h2>
         <p className="mt-1 text-sm/6 text-gray-400">
-          Keep journaling to unlock all hidden achivements!
+          Keep journaling to unlock all hidden achievements!
         </p>
       </div>
       {achievements.map((achievement) => {
         const isUnlocked = unlocked.some(unlockedAchievement => unlockedAchievement.achievement_id === achievement.id);
-        const isHidden = achievement.hidden || !isUnlocked;
+        const isHidden = achievement.hidden;
 
         return (
           <div key={achievement.id} className={`p-4 rounded-lg border ${isUnlocked ? 'border-green-500' : 'border-gray-300'}`}>
