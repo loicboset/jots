4698c884d7dc3e43098fbb8a241c7f1ea7d1d966
Julien Avezou <julien.avezou@gmail.com>
2025-10-24 11:40:07 +0200

    include github integration & data export in release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,81 @@
 /* eslint-disable max-len */
-import type { JSX } from 'react';
+import type { JSX } from "react";
 
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import {
-  ChevronRightIcon
-} from '@heroicons/react/20/solid'
-import { BeakerIcon, BookOpenIcon, RocketLaunchIcon, LightBulbIcon, ArchiveBoxArrowDownIcon, ArrowPathRoundedSquareIcon, BugAntIcon, TrophyIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image';
-import Link from 'next/link'
+  BeakerIcon,
+  BookOpenIcon,
+  RocketLaunchIcon,
+  LightBulbIcon,
+  ArchiveBoxArrowDownIcon,
+  ArrowPathRoundedSquareIcon,
+  BugAntIcon,
+  TrophyIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
 
-import Header from '@/components/collections/layouts/Header';
-import EmailSignupForm from '@/components/EmailSignUpForm';
+import Header from "@/components/collections/layouts/Header";
+import EmailSignupForm from "@/components/EmailSignUpForm";
 
 const primaryFeatures = [
   {
-    name: 'Because You Need a Rubber Duck',
+    name: "Because You Need a Rubber Duck",
     description:
-      'Talk through your thoughts, debug your brain, and solve problems faster with the help of AI.',
-    icon: BeakerIcon
+      "Talk through your thoughts, debug your brain, and solve problems faster with the help of AI.",
+    icon: BeakerIcon,
   },
   {
-    name: 'Because Your PR Needs a Backstory',
+    name: "Because Your PR Needs a Backstory",
     description:
-      'Track your thought process so future-you (and your team) knows why that refactor happened.',
+      "Track your thought process so future-you (and your team) knows why that refactor happened.",
     icon: BookOpenIcon,
   },
   {
-    name: 'Because You Actually Fixed It But Don’t Remember How',
+    name: "Because You Actually Fixed It But Don’t Remember How",
     description:
-      'Log your debugging steps so next time, you have a cheat sheet.',
+      "Log your debugging steps so next time, you have a cheat sheet.",
     icon: BugAntIcon,
   },
   {
-    name: 'Because You Want to See Your Growth',
+    name: "Because You Want to See Your Growth",
     description:
-      'Measure your progress, track achievements, and level up as a developer.',
-    icon: RocketLaunchIcon
+      "Measure your progress, track achievements, and level up as a developer.",
+    icon: RocketLaunchIcon,
   },
   {
-    name: 'Because You Keep Googling the Same Thing',
+    name: "Because You Keep Googling the Same Thing",
     description:
-      'Save your learnings in one place, so you stop asking chatGPT about the same error.',
-    icon: ArrowPathRoundedSquareIcon
+      "Save your learnings in one place, so you stop asking chatGPT about the same error.",
+    icon: ArrowPathRoundedSquareIcon,
   },
   {
-    name: 'Because You’re Juggling Too Many Ideas',
+    name: "Because You’re Juggling Too Many Ideas",
     description:
-      'A developer-friendly space to dump ideas, errors, solutions, and insights.',
+      "A developer-friendly space to dump ideas, errors, solutions, and insights.",
     icon: LightBulbIcon,
   },
   {
-    name: 'Because Your Brain is Full but Your Notebook is Empty',
+    name: "Because Your Brain is Full but Your Notebook is Empty",
     description:
-      'Organize your thoughts, tag important notes, and keep your side projects on track.',
-    icon: ArchiveBoxArrowDownIcon
+      "Organize your thoughts, tag important notes, and keep your side projects on track.",
+    icon: ArchiveBoxArrowDownIcon,
   },
   {
-    name: 'Because You Want That Next Promotion Or Job',
+    name: "Because You Want That Next Promotion Or Job",
     description:
-      'Showcase your achievements to support your promotion case or upcoming job interview.',
-    icon: TrophyIcon
+      "Showcase your achievements to support your promotion case or upcoming job interview.",
+    icon: TrophyIcon,
   },
 ];
-
-
 
 const footerNavigation = {
   social: [
     {
-      name: 'GitHub',
-      href: 'https://github.com/loicboset/DevLogApp',
+      name: "GitHub",
+      href: "https://github.com/loicboset/DevLogApp",
       icon: (): JSX.Element => (
-        <svg fill="currentColor" viewBox="0 0 24 24" className='size-6'>
+        <svg fill="currentColor" viewBox="0 0 24 24" className="size-6">
           <path
             fillRule="evenodd"
             d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -80,16 +85,23 @@ const footerNavigation = {
       ),
     },
     {
-      name: 'Discord',
-      href: 'https://discord.gg/YQP4wwbyfg',
+      name: "Discord",
+      href: "https://discord.gg/YQP4wwbyfg",
       icon: (): JSX.Element => (
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="size-8" viewBox="0 0 24 24">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          className="size-8"
+          viewBox="0 0 24 24"
+        >
           <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
         </svg>
       ),
     },
   ],
-}
+};
 
 const LandingPage = (): JSX.Element => {
   return (
@@ -100,28 +112,46 @@ const LandingPage = (): JSX.Element => {
           <Header />
           <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
             <div className="mx-auto max-w-3/4 shrink-0 lg:mx-0 lg:pt-8">
-              <div className='flex justify-between items-center flex-wrap space-y-4'>
+              <div className="flex justify-between items-center flex-wrap space-y-4">
                 <Link href="/releases" className="inline-flex space-x-6">
                   <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm/6 font-semibold text-indigo-400 ring-1 ring-indigo-500/20 ring-inset">
                     What&apos;s new
                   </span>
                   <span className="inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300">
-                    <span className='hidden md:inline'>Just shipped v2025.08.19</span>
-                    <ChevronRightIcon aria-hidden="true" className="size-5 text-gray-500" />
+                    <span className="hidden md:inline">
+                      Just shipped v2025.10.24
+                    </span>
+                    <ChevronRightIcon
+                      aria-hidden="true"
+                      className="size-5 text-gray-500"
+                    />
                   </span>
                 </Link>
-                <a href="https://www.producthunt.com/products/jots?embed=true&utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_source=badge-jots" target="_blank" rel="noreferrer">
-                  <Image src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=971956&theme=light&period=weekly&topic_id=46&t=1749590789894" alt="Jots - Unlock&#0032;the&#0032;benefits&#0032;of&#0032;journaling&#0032;to&#0032;become&#0032;a&#0032;better&#0032;dev&#0046; | Product Hunt" style={{ width: "250px", height: "54px" }} width="250" height="54" />
+                <a
+                  href="https://www.producthunt.com/products/jots?embed=true&utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_source=badge-jots"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=971956&theme=light&period=weekly&topic_id=46&t=1749590789894"
+                    alt="Jots - Unlock&#0032;the&#0032;benefits&#0032;of&#0032;journaling&#0032;to&#0032;become&#0032;a&#0032;better&#0032;dev&#0046; | Product Hunt"
+                    style={{ width: "250px", height: "54px" }}
+                    width="250"
+                    height="54"
+                  />
                 </a>
               </div>
               <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl">
                 Level Up Your Dev Game With Jots 🚀
               </h1>
               <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-                Unlock the benefits of journaling to become a better, faster developer every day.
+                Unlock the benefits of journaling to become a better, faster
+                developer every day.
               </p>
               <p className="mt-8 text-lg font-medium text-pretty text-orange-300 sm:text-xl/8">
-                We are releasing the app for early adopters to help shape the future of Jots. Join our beta phase now for exclusive free access to all our features, feedback loops, and community.
+                We are releasing the app for early adopters to help shape the
+                future of Jots. Join our beta phase now for exclusive free
+                access to all our features, feedback loops, and community.
               </p>
               <div className="mt-10 flex flex-wrap space-y-2 items-center gap-x-6">
                 <Link
@@ -142,7 +172,8 @@ const LandingPage = (): JSX.Element => {
         <div className="mx-auto mt-2 max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">
-              Why Do Top Developers Keep a Journal,<span className='text-indigo-400'> and Why You Should Too?</span>
+              Why Do Top Developers Keep a Journal,
+              <span className="text-indigo-400"> and Why You Should Too?</span>
             </p>
           </div>
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
@@ -151,9 +182,14 @@ const LandingPage = (): JSX.Element => {
                 <div key={feature.name} className="flex flex-col">
                   <dt className="flex space-x-4 items-center mb-4">
                     <div className="flex shrink-0 h-10 w-10 items-center justify-center rounded-lg bg-indigo-500">
-                      <feature.icon aria-hidden="true" className="h-6 w-6 text-white" />
+                      <feature.icon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-white"
+                      />
                     </div>
-                    <span className='font-semibold text-white text-2xl'>{feature.name}</span>
+                    <span className="font-semibold text-white text-2xl">
+                      {feature.name}
+                    </span>
                   </dt>
                   <dd className="mt-1 flex flex-auto flex-col text-base/7 text-gray-300">
                     <p className="flex-auto">{feature.description}</p>
@@ -168,37 +204,37 @@ const LandingPage = (): JSX.Element => {
         <div className="mx-auto mt-24 max-w-7xl px-6 lg:px-8">
           <div className="mx-auto mb-12 max-w-2xl lg:text-center">
             <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">
-              See Jots in<span className='text-indigo-400'> action</span>
+              See Jots in<span className="text-indigo-400"> action</span>
             </p>
           </div>
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
             <div
               style={{
-                position: 'relative',
-                paddingBottom: '56.25%',
-                width: '80%',
-                maxWidth: '800px',
+                position: "relative",
+                paddingBottom: "56.25%",
+                width: "80%",
+                maxWidth: "800px",
                 height: 0,
-                border: '4px solid black',
-                borderRadius: '8px',
-                overflow: 'hidden',
+                border: "4px solid black",
+                borderRadius: "8px",
+                overflow: "hidden",
               }}
             >
               <iframe
                 src="https://www.loom.com/embed/591f4740ecaa4de0a4903bcc30030ac5?sid=f22a9e2d-d155-4f99-af11-0620aff09100"
                 style={{
-                  position: 'absolute',
+                  position: "absolute",
                   top: 0,
                   left: 0,
-                  width: '100%',
-                  height: '100%',
-                  border: '0',
+                  width: "100%",
+                  height: "100%",
+                  border: "0",
                 }}
                 allowFullScreen
                 title="Jots demo video"
@@ -231,7 +267,12 @@ const LandingPage = (): JSX.Element => {
                 strokeWidth={0}
               />
             </svg>
-            <rect fill="url(#1d4240dd-898f-445f-932d-e2872fd12de3)" width="100%" height="100%" strokeWidth={0} />
+            <rect
+              fill="url(#1d4240dd-898f-445f-932d-e2872fd12de3)"
+              width="100%"
+              height="100%"
+              strokeWidth={0}
+            />
           </svg>
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
@@ -255,7 +296,12 @@ const LandingPage = (): JSX.Element => {
               <h2>✨ VSCode Extension</h2>
               <p>Log your developer journal right from VSCode!</p>
               <div className="mt-4">
-                <a href="https://marketplace.visualstudio.com/items?itemName=Jots.daily-jots" rel="noreferrer" target="_blank" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 cursor:pointer">
+                <a
+                  href="https://marketplace.visualstudio.com/items?itemName=Jots.daily-jots"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 cursor:pointer"
+                >
                   <button>Install VSCode Extension</button>
                 </a>
               </div>
@@ -275,7 +321,11 @@ const LandingPage = (): JSX.Element => {
               Privacy
             </Link>
             {footerNavigation.social.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-gray-400 hover:text-gray-300"
+              >
                 <span className="sr-only">{item.name}</span>
                 <item.icon aria-hidden="true" />
               </a>
@@ -287,7 +337,7 @@ const LandingPage = (): JSX.Element => {
         </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
